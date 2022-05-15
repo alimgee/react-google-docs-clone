006477f4af5640bf1be077703b9f0e336630cc95
alanmcgee <mcgee.al@gmail.com>
2022-05-15 13:58:50 +0100

    install firebase, set up firebase config and set up Docs / Modal to add data to firebase

--- src/components/Docs.js
+++ src/components/Docs.js
@@ -1,9 +1,28 @@
 import React, { useState } from 'react';
 import Modal from './Modal';
+import { addDoc, collection } from 'firebase/firestore';
 
-export default function Docs() {
+
+export default function Docs({
+    database
+}) {
+    const collectionRef = collection(database, 'docsData')
     const [open, setOpen] = React.useState(false);
+    const [title, setTitle] = useState('')
+
     const handleOpen = () => setOpen(true);
+    const addData = () => {
+        addDoc(collectionRef, {
+            title: title
+        })
+            .then(() => {
+                alert('Data Added')
+            })
+            .catch(() => {
+                alert('Cannot add data')
+            })
+    }
+
     return (
         <div className='docs-main'>
             <h1>Docs Clone</h1>
@@ -18,8 +37,9 @@ export default function Docs() {
             <Modal
                 open={open}
                 setOpen={setOpen}
-            />
-
+                title={title}
+                setTitle={setTitle}
+                addData={addData} />
         </div>
     )
 }
